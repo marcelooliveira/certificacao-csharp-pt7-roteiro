092d1f80f561533e66efbd15cf6ce9072daaa297
marcelooliveira <mclricardo@gmail.com>
2018-11-07 18:25:48 -0200

    .

---
+++ certificacao-csharp-pt7-roteiro/01/01.04/depois/ex02.feature.cs
@@ -0,0 +1,6 @@
+﻿#error Parsing error near 'You are implementing a method that creates an instance of a class named User. The User\'
+#error Parsing error near 'class contains a public event named Renamed. The following code segment defines the\'
+#error Parsing error near 'Renamed event:\'
+#error Parsing error near 'Public event EventHandler<RenameEventArgs> Renamed;\'
+#error Parsing error near 'You need to create an event handler for the Renamed event by using a lambda expression.\'
+#error Parsing error near 'How should you complete the relevant code? (To answer, drag the appropriate code\'
