3d860a413f4eeae490223b874ace03cd6bebc8a3
marcelooliveira <mclricardo@gmail.com>
2018-11-16 15:32:43 -0200

    .

---
+++ certificacao-csharp-pt7-roteiro/01/01.02/antes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _01_02
+{
+    class Program
+    {
+        static void CampainhaTocou1()
+        {
+            Console.WriteLine("A campainha tocou.");
+        }
+
+        static void CampainhaTocou2()
+        {
+            Console.WriteLine("A campainha tocou.");
+        }
+
+        static void Main(string[] args)
+        {
+            Campainha campainha = new Campainha();
+            //campainha.Tocar();
+            //campainha.OnCampainhaTocou = new Action(() => { });
+            campainha.OnCampainhaTocou += CampainhaTocou1;
+            campainha.OnCampainhaTocou += CampainhaTocou2;
+
+            campainha.Tocar();
+            Console.ReadKey();
+        }
+    }
+
+    class Campainha
+    {
+        public Action OnCampainhaTocou { get; set; }
+
+        public void Tocar()
+        {
+            //if (OnCampainhaTocou != null)
+            //{
+            //    OnCampainhaTocou();
+            //}
+
+            OnCampainhaTocou?.Invoke();
+        }
+    }
+
+}
